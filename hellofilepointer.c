d54a3c76cbb7ee0e624c17d94f6aff464884186e
Nataliia Timoshyna <nezhinskaya83@gmail.com>
2020-09-22 15:15:43 +0300

    Update hellofilepointer.c

--- hellofilepointer.c
+++ hellofilepointer.c
@@ -18,3 +18,15 @@ int main(void) {
     fclose(file); //Закриваємо файл
   }
 }
+
+// то же только с fprintf
+// #include <stdio.h>
+// int main(void)
+// {
+//   FILE* file = fopen("hello", "w");
+//   if (file != NULL)
+//   {
+//     fprintf(file, "Hello, world!");
+//     fclose(file);
+//   }
+// }
